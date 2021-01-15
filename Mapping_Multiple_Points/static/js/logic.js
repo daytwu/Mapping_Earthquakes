9ebdf497792f1d921a0c41943b5250c9275362d6
daytwu <daytwu@gmail.com>
2021-01-15 16:16:42 -0500

    updated

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,14 +2,18 @@
 console.log('working');
 
 //  Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 5);
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 
-// Add a marker to the map for Los Angeles, California
-// L.circleMarker([34.0522, -118.2437],{
-//         radius:300,
-//         color: 'black',
-//         fillColor: '#ffffa1'
-//     }).addTo(map);
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790]
+];
+
+// Create a polyline using the line coordinates and make the line ReadableByteStreamController.apply
+L.polyline(line, {
+    color: 'red'
+}).addTo(map);
 
 // Get data from cities.js
 let cityData = cities;
