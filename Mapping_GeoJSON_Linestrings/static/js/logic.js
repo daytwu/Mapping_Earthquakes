3c270c7704394526a1eec5ea181482d236150c41
daytwu <daytwu@gmail.com>
2021-01-15 20:06:08 -0500

    updated

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -30,13 +30,22 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/daytwu/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJSON_Points/static/js/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/daytwu/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/static/js/torontoRoutes.json";
 
 //  Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
-    // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data).addTo(map);
+
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+    color: '#ffffa1',
+    weight: 2,
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr> Destination: "
+        + feature.properties.dst + "</h3>");
+    }
+})
+.addTo(map);
 });
 
 // Then we add our 'graymap' tile layer to the map.
